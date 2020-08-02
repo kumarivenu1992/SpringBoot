fb49740ef6fa5acb2b3952fa9df66226874d43bd
Venu.K <Venu.K@Venu>
2020-08-02 14:40:00 +0530

    initial commit

---
+++ app1-springboot-h2db-curd-operations/src/main/java/com/example/demo/App1SpringbootH2dbCurdOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App1SpringbootH2dbCurdOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App1SpringbootH2dbCurdOperationsApplication.class, args);
+	}
+
+}
